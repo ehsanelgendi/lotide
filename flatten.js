6fad9ccd0d7528884502f39441bdb6ac41af1414
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-09-27 14:49:07 +0000

    adding flatten.js

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const flatten = function(array) {
+  let output = [];
+  array.forEach(function(element) {
+    if (Array.isArray(element)) {
+      element.forEach(function(value) {
+        output.push(value);
+      })
+    } else {
+      output.push(element);
+    }
+  });
+  return output;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
