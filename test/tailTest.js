6b5693875dcfbf7b18575d53adbb26903b4ae3f4
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-16 01:55:28 +0000

    revisiting tailTest.js & middleTest.js with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,25 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Ehsan", "Ehsan");
-assertEqual(1, 2);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const arr1element = ["Yo Yo"];
-tail(arr1element);
-assertEqual(arr1element.length, 1);
-
-const emptyArr = [];
-tail(emptyArr);
-assertEqual(emptyArr.length, 0);
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2); 
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']); 
+  });
+});
