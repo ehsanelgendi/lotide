8146071a62fd6e4869d2b5deb1a8c2e5a03759b6
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-16 06:06:51 +0000

    removing commented lines

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,16 +1,6 @@
 const assert = require('chai').assert;
 const without = require('../without');
 
-// console.log(without([1, 2, 3], [1])); // => [2, 3]
-// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-
-// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); 
-
-// const words = ["hello", "world", "lighthouse"];
-// console.log(without(words, ["lighthouse"]));
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
 describe("#without", () => {
   it("returns [2, 3] when passes [1, 2, 3], [1]", () => {
     assert.deepEqual(without([1, 2, 3], [1]),  [2, 3]);
