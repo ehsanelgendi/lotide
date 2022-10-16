9edca3a24bea8047d7496b6e17be093edcf051ec
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-16 05:11:50 +0000

    refactoring findKey & findKeyByValue with mocha & chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, value) {
   // using for...in
   // for (const key in obj) {
@@ -24,23 +16,4 @@ const findKeyByValue = function(obj, value) {
   }
 };
 
-// TEST CODE
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, " "), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
-
-const bestTVShowsByGenre2 = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  horror: "",
-  drama2:  "The Wire"
-};
+module.exports = findKeyByValue;
---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+describe("#findKeyByValue", () => {
+  const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    horror: "",
+    drama:  "The Wire"
+  };
+
+  it("returns genre 'drama' when passed object and movie name 'The Wire'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+  it("returns genre 'horror' when passed object and movie name as empty string", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, ""), "horror");
+  });
+  it("returns genre undefined when passed object and movie name as a space", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, " "), undefined);
+  });
+});
