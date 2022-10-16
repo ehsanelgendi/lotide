f7c12290a7a31af067afe763f58e028fa9dab87c
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-16 05:17:48 +0000

    refactoring flatten.js with mocha & chai

--- flatten.js
+++ flatten.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
 const flatten = function(array) {
   let output = [];
   array.forEach(function(element) {
@@ -32,5 +12,4 @@ const flatten = function(array) {
   return output;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flatten;
