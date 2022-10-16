a8e3b62b0379235e6749ca6f0974b88cd293e586
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-16 05:35:23 +0000

    remove commented lines from letterPositionsTest

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,14 +1,6 @@
 const assert = require('chai').assert;
 const letterPositions = require('../letterPositions');
 
-// console.log(letterPositions("hello"));
-// console.log(letterPositions("lighthouse in the house"));
-
-// assertArraysEqual(letterPositions("hello").h, [0]);
-// assertArraysEqual(letterPositions("hello").e, [1]);
-// assertArraysEqual(letterPositions("hello").l, [2,3]);
-// assertArraysEqual(letterPositions("hello").o, [4]);
-
 describe('#letterPositions', () => {
   it("returns [0] for letter 'h' in a string 'hello'", () => {
     assert.deepEqual(letterPositions("hello").h, [0]);
